6f89442dc02f53a5fcfea42e2a3185d36b9db608
ubik <322449+himynameisubik@users.noreply.github.com>
2024-07-23 23:31:29 +0200

    update
    - added check for existing thumbnails
    - added --force arg to regenerate existing thumbnails incl. confirmation
    - added --blur_radius and --thumb_quality as args
    - "beautification" of the print messages
    - cleanup
    - updated readme

--- README.md
+++ README.md
@@ -1,25 +1,32 @@
 # Plex Generate Spoiler Free Thumbnails
- Simple python script to create spoiler free (blurred) thumbnails for Plex TV Shows
+Python script to create spoiler free (blurred) thumbnails for Plex TV Shows
 
 # Supported folder structure
- Both loose and season folder structure works
- - `.../The Office/Season X/...(files are here)`
- - `.../The Office/...(files are here)` 
+Both loose and season folder structure works
+-  `.../The Office/Season X/...(files are here)`
+-  `.../The Office/...(files are here)`
 
 # Supported file types
- The usuals, probably could be extended(?)
-- `.mkv`
-- `.mp4`
-- `.avi`
+The usuals, probably could be extended(?)
+-  `.mkv`
+-  `.mp4`
+-  `.avi`
 
 # How to use
- 1. Install pillow: `pip install pillow`
- 2. Copy paste `generate-thumbs.py` in to your desired TV shows folder (e.g. `X:\TV Shows\The Office`)
- 3. Open Terminal and cd into the folder you copied `generate-thumbs.py` to (e.g. `cd "X:\TV Shows\The Office"`)
- 4. Run `python generate-thumbs.py` and wait
- 5. ...
- 6. Profit
+1. Install pillow: `pip install pillow`
+2. Copy paste `generate-thumbs.py` in to your desired TV shows folder (e.g. `X:\TV Shows\The Office`)
+3. Open Terminal and cd into the folder you copied `generate-thumbs.py` to (e.g. `cd "X:\TV Shows\The Office"`)
+4. Run `python generate-thumbs.py` and wait
+5. ...
+6. Profit
 
-# Configuration
- - Line #68: configure the amount of blur for the thumbnail (default: 100)
- - Line #71: configure the quality of the thumbnail picture (default: 80)
+# Additional configuration
+The following arguments are available:
+
+|Argument|Information|
+|-|-|
+|`--force`|Forces the regeneration of existing thumbnails|
+|`--blur_radius 100`|Changes the blur amount (default: 100)|
+|`--thumb_quality 80`|Changes the quality (default: 80)|
+
+e.g. `python generate-thumbs.py --force --blur_radius 50 --thumb_quality 60`
--- generate-thumbs.py
+++ generate-thumbs.py
@@ -1,5 +1,5 @@
 import os
-import random
+import argparse
 import cv2
 from PIL import Image, ImageFilter
 
@@ -48,31 +48,62 @@ def process_video(video_path, blur_radius):
     except Exception as e:
         print(f"Error processing video file {video_path}: {e}")
         return None
-        
-# Main function to process TV show folders
-def process_tv_shows(root_folder, blur_radius):
+
+# Main function to process video folders
+def process_video_files(root_folder, blur_radius, thumb_quality, force):
+    print(f"\n")
+    # Print "Start" message on process start
+    print(f"Starting spoiler-free thumbnail generation...")
+    print(f"\n")
+
     for root, dirs, files in os.walk(root_folder):
         for file in files:
             if file.endswith((".mp4", ".mkv", ".avi")):
                 video_path = os.path.join(root, file)
+                jpg_file_name = os.path.splitext(file)[0] + ".jpg"
+                jpg_file_path = os.path.join(root, jpg_file_name)
+
+                # Check if the thumbnail already exists
+                if not force and os.path.exists(jpg_file_path):
+                    # Print message that generation is skipped because thumbnail already exists
+                    print(f"  - Thumbnail already exists for {file}, skipping...\n")
+                    continue
+                
+                # Print message with active thumbnail generation
+                print(f"  + Creating thumbnail for {file}...")
                 blurred_image = process_video(video_path, blur_radius)
                 if blurred_image is not None:
-                    jpg_file_name = os.path.splitext(file)[0] + ".jpg"
-                    blurred_image.save(os.path.join(root, jpg_file_name), quality=thumb_quality)  # Adjust quality level as needed
-                    print(f"Blurred image created for {file}")
+                    blurred_image.save(os.path.join(root, jpg_file_name), quality=thumb_quality)
+                    print(f"  = Thumbnail created for {file}\n")
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Generate spoiler-free thumbnails for video files.")
+parser.add_argument("--force", action="store_true", help="Force thumbnail creation even if they already exist.")
+parser.add_argument("--blur_radius", type=int, default=100, help="Set the radius of the blur effect (default: 100).")
+parser.add_argument("--thumb_quality", type=int, default=80, help="Set the quality of the thumbnail (default: 80).")
+args = parser.parse_args()
+
+# Ask for confirmation if -force is provided
+if args.force:
+    print(f"\n")
+    print(f"Warning: Argument -force was set, existing thumbnails will be regenerated...")
+
+    confirmation = input("Are you sure you want to regenerate existing thumbnails? (Y/n): ")
+    if confirmation.lower() != 'y':
+        print("Operation cancelled.")
+        exit()
 
 # Set the root folder
 root_folder = os.path.dirname(os.path.abspath(__file__))
 
-# Define the blur radius
-blur_radius = 100   # Adjust this value to control the amount of blur
+# Args
+blur_radius = args.blur_radius
+thumb_quality = args.thumb_quality
+force = args.force
 
-# Define thumbnail quality
-thumb_quality = 80  # Adjust this value to control the quality of the thumbnail
-
-# Call the function to process TV show folders
-process_tv_shows(root_folder, blur_radius)
+# Call the function to process video files
+process_video_files(root_folder, blur_radius, thumb_quality, force)
 
 # Print "Done" message after processing is complete
-print()
-print("-- All images processed successfully.")
+print(f"\nSuccessfully processed all video files. Happy spoiler-free watching! :)")
+print(f"\n")
